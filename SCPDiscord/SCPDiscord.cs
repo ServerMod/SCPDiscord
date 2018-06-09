46294901951be80c04ab2a5e11e3f1f2f5b336a5
Karl Essinger <xkaess22@gmail.com>
2018-06-09 07:00:38 +0200

    Adapted example plugin as a base

---
+++ SCPDiscord/SCPDiscord.cs
@@ -0,0 +1,34 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace SCPDiscord
+{
+    [PluginDetails(
+        author = "Karl Essinger",
+        name = "SCPDiscord",
+        description = "Plugin which outputs server events to Discord.",
+        id = "karlofduty.scpdiscord",
+        version = "0.1",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    internal class ExamplePlugin : Plugin
+    {
+        public override void OnDisable()
+        {
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("SCPDiscord enabled.");
+            this.Info("Config value: " + this.GetConfigString("test"));
+        }
+
+        public override void Register()
+        {
+        }
+    }
+}
