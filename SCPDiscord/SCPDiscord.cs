942cb67a7a87c51f3ce233d3a2fee6ed8ba93ad6
Karl Essinger <xkaess22@gmail.com>
2018-06-10 02:50:08 +0200

    Added DSharpPlus library in preparation of the next game update

--- SCPDiscord/SCPDiscord.cs
+++ SCPDiscord/SCPDiscord.cs
@@ -3,6 +3,9 @@ using Smod2.Attributes;
 using Smod2.EventHandlers;
 using Smod2.Events;
 
+using DSharpPlus;
+using System.Threading.Tasks;
+
 namespace SCPDiscord
 {
     [PluginDetails(
@@ -10,13 +13,15 @@ namespace SCPDiscord
         name = "SCPDiscord",
         description = "Plugin which outputs server events to Discord.",
         id = "karlofduty.scpdiscord",
-        version = "0.1",
+        version = "3.0",
         SmodMajor = 3,
         SmodMinor = 0,
         SmodRevision = 0
         )]
     internal class ExamplePlugin : Plugin
     {
+        private static DiscordClient discord;
+
         public override void OnDisable()
         {
         }
@@ -24,7 +29,25 @@ namespace SCPDiscord
         public override void OnEnable()
         {
             this.Info("SCPDiscord enabled.");
-            this.Info("Config value: " + this.GetConfigString("test"));
+        }
+
+        static public async Task TestFunction()
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "token",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("!ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
 
         public override void Register()
