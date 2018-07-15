8cb500a7d75e316547c7e5dc664931002c2ac043
Karl Essinger <xkaess22@gmail.com>
2018-07-15 18:28:37 +0200

    Refactoring solution into two projects

---
+++ SCPDiscordPlugin/SCPDiscord.cs
@@ -0,0 +1,34 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace SCPDiscord
+{
+    [PluginDetails(
+        author = "Karl Essinger",
+        name = "SCPDiscord",
+        description = "Plugin which outputs server events to Discord.",
+        id = "karlofduty.scpdiscord",
+        version = "3.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    class ExamplePlugin : Plugin
+    {
+
+        public override void OnDisable()
+        {
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("SCPDiscord enabled.");
+        }
+
+        public override void Register()
+        {
+        }
+    }
+}
