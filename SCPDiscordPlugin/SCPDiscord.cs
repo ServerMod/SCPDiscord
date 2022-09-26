0a003b01569434b4511c70ef0ca4cb79e16d1aa6
Karl Essinger <xkaess22@gmail.com>
2022-09-26 22:22:35 +0200

    Version bump

--- SCPDiscordPlugin/SCPDiscord.cs
+++ SCPDiscordPlugin/SCPDiscord.cs
@@ -25,7 +25,7 @@ namespace SCPDiscord
 		name = "SCPDiscord",
 		description = "SCP:SL - Discord bridge.",
 		id = "karlofduty.scpdiscord",
-		version = "2.0.6-B",
+		version = "2.1.0",
 		SmodMajor = 3,
 		SmodMinor = 10,
 		SmodRevision = 5
